23f72fe8f482c8755a728a8752a6faf812045e65
aedalton16 <a.e.dalton.16@gmail.com>
2015-12-06 03:10:13 -0600

    pre-overhaul

--- routes/index.js
+++ routes/index.js
@@ -33,7 +33,7 @@ module.exports = function(app, passport) {
         res.render('signup', { message: 'login' });
     });
 
-       app.post('/signup', passport.authenticate('local-signup', {
+    app.post('/signup', passport.authenticate('local-signup', {
         successRedirect : '/profile', // redirect to the secure profile section
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
